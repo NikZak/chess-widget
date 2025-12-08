6b21068555010b516f591e5a2cbddb0f93c92ce0
NikZak <nickzakirov@gmail.com>
2025-12-08 23:44:00 +0800

    fix

--- config.js
+++ config.js
@@ -24,7 +24,7 @@ const translations = {
   },
 };
 
-// Default puzzles
+// Default puzzles (Long Algebraic Notation - LAN)
 const DEFAULT_PUZZLES = [
   {
     fen: "r3k2r/1b1p1pp1/3qp3/p1bP4/Pp3B2/6P1/1PP1QPB1/R4RK1 b kq - 0 1",
@@ -44,6 +44,27 @@ const DEFAULT_PUZZLES = [
   },
 ];
 
+// Default puzzles with Short Algebraic Notation (SAN) - for testing notation converter
+// Same puzzles as above but using SAN notation like "Qxd5", "Nf3", etc.
+const DEFAULT_PUZZLES_2 = [
+  {
+    fen: "r3k2r/1b1p1pp1/3qp3/p1bP4/Pp3B2/6P1/1PP1QPB1/R4RK1 b kq - 0 1",
+    moves: "Qxd5,Bxd5,Bxd5,Rad1,Rh1+",
+    message: "Найдите выигрывающую комбинацию",
+  },
+  {
+    fen: "3n3r/2p3k1/1pbbpRpp/6r1/3P4/2PBQ2P/q5P1/5RK1 w - - 0 1",
+    moves:
+      "Qxg5+,hxg5,Rxg6+,Kh7,Rxe6,Kg7,Rg6+,Kh7,Rxd6,Kg7,Rg6+,Kh7,Rxc6,Kg7,Rg6+,Kh7,Rxb6,Kg7,Rg6+,Kh7,Ra6,Kg7,Rxa2",
+    message: "Найдите выигрывающую комбинацию",
+  },
+  {
+    fen: "r2q2rk/ppp4p/3p4/2b2Q2/3pPPR1/2P2n2/PP3P1P/RNB4K b - - 0 1",
+    moves: "Qh4,Rxh4,Rg1#", // Qh4 not Qxh4 - h4 is empty, no capture
+    message: "Найдите выигрывающий ход",
+  },
+];
+
 // Parse puzzles from URL
 function parsePuzzlesFromURL() {
   const urlParams = new URLSearchParams(window.location.search);
@@ -51,10 +72,12 @@ function parsePuzzlesFromURL() {
   const fenParam = urlParams.get("fen");
   const movesParam = urlParams.get("moves");
 
+  let puzzles;
+
   if (puzzlesParam) {
     // New format: multiple puzzles separated by ;
     const puzzleStrings = puzzlesParam.split(";");
-    return puzzleStrings.map((puzzleStr) => {
+    puzzles = puzzleStrings.map((puzzleStr) => {
       const parts = puzzleStr.split("|");
       return {
         fen: decodeURIComponent(parts[0] || DEFAULT_PUZZLES[0].fen),
@@ -65,7 +88,7 @@ function parsePuzzlesFromURL() {
   } else if (fenParam || movesParam) {
     // Legacy format: single puzzle
     const messageParam = urlParams.get("message") || DEFAULT_PUZZLES[0].message;
-    return [
+    puzzles = [
       {
         fen: fenParam || DEFAULT_PUZZLES[0].fen,
         moves: movesParam || DEFAULT_PUZZLES[0].moves,
@@ -73,9 +96,16 @@ function parsePuzzlesFromURL() {
       },
     ];
   } else {
-    // No parameters: use default puzzles
-    return DEFAULT_PUZZLES;
+    // Use DEFAULT_PUZZLES_2 (SAN notation test set)
+    puzzles = DEFAULT_PUZZLES;
   }
+
+  // Normalize all puzzles to LAN format (converts SAN to LAN if needed)
+  if (window.NotationConverter && window.NotationConverter.normalizePuzzles) {
+    return window.NotationConverter.normalizePuzzles(puzzles);
+  }
+
+  return puzzles;
 }
 
 // Get language from URL
